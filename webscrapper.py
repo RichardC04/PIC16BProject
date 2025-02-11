dd6f6b47eeb48baa33f700782dda104eafc9e2e1
ljx2021s <ljx2021s@g.ucla.edu>
2025-02-11 02:25:01 -0800

    Update webscrapper.py

--- webscrapper.py
+++ webscrapper.py
@@ -5,11 +5,10 @@ from bs4 import BeautifulSoup
 import time
 import pandas as pd
 
-# Set up Selenium (Chrome in this example)
+# Set up Selenium
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get("https://lol.fandom.com/wiki/LCK/2024_Season/Spring_Season/Player_Statistics")
 
-# Wait for JavaScript to load the content (adjust the sleep time as needed)
 time.sleep(5)
 
 # Get page source and parse it with BeautifulSoup
@@ -21,8 +20,6 @@ table = soup.select_one("table.wikitable.sortable.spstats.plainlinks.hoverable-r
 if table:
     print("Table found!")
     
-    # --- Method 1: Extract with BeautifulSoup ---
-    print("\nExtracting data using BeautifulSoup:")
     rows = table.find_all('tr')
     table_data = []
     for row in rows:
@@ -31,18 +28,12 @@ if table:
         if cell_texts:
             table_data.append(cell_texts)
     
-    # Print each row of the extracted data
-    #for row in table_data:
-        #print(row)
-
     result = {}
     for i in range(5, len(table_data)):
         temp = {}
         for j in range(2, 21):
             temp[f'{table_data[4][j]}'] = table_data[i][j]
         result[f'{table_data[i][1]}'] = temp
-
-    # print(result)
         
 else:
     print("Table not found.")
